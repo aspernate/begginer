a5bf706b4950e00561418bfc9963987ec7634375
aspernate <tonylon@mail.ru>
2016-11-01 21:59:01 +0300

    doing level28.lesson15.big01

--- level28/lesson15/big01/model/HHStrategy.java
+++ level28/lesson15/big01/model/HHStrategy.java
@@ -1,7 +1,11 @@
 package com.javarush.test.level28.lesson15.big01.model;
 
 import com.javarush.test.level28.lesson15.big01.vo.Vacancy;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.List;
 
 /**
@@ -13,6 +17,14 @@ public class HHStrategy implements Strategy {
 
     @Override
     public List<Vacancy> getVacancies(String searchString) {
+        try
+        {
+            Document doc = Jsoup.connect(String.format(URL_FORMAT, "Киев", 3))
+                    .userAgent("Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36")
+                    .referrer("http://google.ru")
+                    .get();
+        }
+        catch (IOException e) {}
         return null;
     }
 }
