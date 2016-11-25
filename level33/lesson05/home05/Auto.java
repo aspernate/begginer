5e83072ee5d577a27fa44d4eea4ec3491335bda2
aspernate <tonylon@mail.ru>
2016-11-25 18:56:49 +0300

    doing lvl33

--- level33/lesson05/home05/Auto.java
+++ level33/lesson05/home05/Auto.java
@@ -1,5 +1,8 @@
 package com.javarush.test.level33.lesson05.home05;
 
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.CLASS,  property="className")
 public abstract class Auto {
     protected String name;
     protected String owner;
